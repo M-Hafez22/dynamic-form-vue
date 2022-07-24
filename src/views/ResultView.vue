e0b47b32c4d845f6f1a7a1f2585924e5dc4f2f4d
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-24 11:04:44 +0200

    ✅ Renders data from store

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,6 +1,15 @@
+<script setup>
+import { useFormStore } from "../stores/form";
+
+const store = useFormStore();
+</script>
+
 <template>
   <div class="Result">
     <h1>Your Form Input</h1>
+        <div v-for="item in store.form" :key="item.name">
+      <p>{{item.name}}: <span>{{item.value}}</span></p>
+    </div>
   </div>
 </template>
 
