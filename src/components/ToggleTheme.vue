1818fc164ed25ea9671473f45897fc3aad934799
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-24 11:26:56 +0200

    💄 Move buttons to right bottom

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -5,24 +5,26 @@ const store = useThemeStore();
 </script>
 
 <template>
-  <div class="theme-btns">
-    <input
-      :class="store.theme"
-      type="radio"
-      id="Dark"
-      name="Dark"
-      label="Dark"
-      :checked="store.theme === 'dark'"
-      @click="store.toggleTheme('dark')"
-    />
-    <input
-      :class="store.theme"
-      type="radio"
-      id="Light"
-      label="Light"
-      name="Light"
-      :checked="store.theme === 'light'"
-      @click="store.toggleTheme('light')"
-    />
+  <div class="themes">
+    <div class="theme-btns">
+      <input
+        :class="store.theme"
+        type="radio"
+        id="Dark"
+        name="Dark"
+        label="Dark"
+        :checked="store.theme === 'dark'"
+        @click="store.toggleTheme('dark')"
+      />
+      <input
+        :class="store.theme"
+        type="radio"
+        id="Light"
+        label="Light"
+        name="Light"
+        :checked="store.theme === 'light'"
+        @click="store.toggleTheme('light')"
+      />
+    </div>
   </div>
 </template>
