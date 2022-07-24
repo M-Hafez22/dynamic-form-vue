138fe0082dbfe823a15e1fd9fec4e390b7a9933d
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-24 10:26:58 +0200

    Add form store

---
+++ src/stores/form.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+
+export const useFormStore = defineStore({
+  id: 'form',
+  state: () => ({
+    form: [
+      { name: "name", value: '', type: "text" },
+      { name: "email", value: '', type: "email" },
+      { name: "address", value: '', type: "text" },
+      { name: "skills", value: '', type: "text" },
+      { name: "password", value: '', type: "password" },
+      { name: 'CV', value: '', type: 'file' }
+    ]
+  }),
+  actions: {
+
+  }
+})
